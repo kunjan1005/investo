96fac4e2c112e4b39373aced016370bbcaa73609
kunjan1005 <barotkunjan1312@gmail.com>
2022-02-17 12:12:17 +0530

    first push

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import Login from './components/Login';
+
+const  App=()=>{
+  return (
+    <>
+
+     <Login/>
+
+    </>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import './App.css'
+import App from './App';
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
+ReactDOM.render(
+ 
+    <App />
+  
+  ,document.getElementById('root')
+);
+
+
