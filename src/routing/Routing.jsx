ade313337f78eeddacbf9febb54335a87fa6961f
samrat-barot <samrat.saubhagyam@gmail.com>
2022-02-22 12:19:15 +0530

    login page -s

--- src/routing/Routing.jsx
+++ src/routing/Routing.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Route, Routes, Router } from 'react-router';
 import PageNotFound from '../404Error/PageNotFound';
 import Dashboard from '../components/Dashboard';
+import Singup from '../components/Login';
+import Login from '../components/Login';
 import CustomerAccount from '../customers/account/CustomerAccount';
 import MainCustomerPage from '../customers/MainCustomerPage';
 import OrderList from '../trading/OrderList';
@@ -9,6 +11,9 @@ import OrderList from '../trading/OrderList';
 const Routing = () => {
     return (<>
         <Routes>
+            <Route excat path='/login' element={<Login/>} />
+            <Route excat path='/signup' element={<Singup />} />
+            
             <Route excat path='/' element={<Dashboard />} />
             <Route activeClass='active' excat path='/customer' element={<MainCustomerPage />} />
             <Route excat path='/customer/account' element={<CustomerAccount />} />
