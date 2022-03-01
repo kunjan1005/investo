f90527f02ddc4f08dd6f77be2fbdb3811ebc7e76
kunjan1005 <barotkunjan1312@gmail.com>
2022-03-01 11:28:56 +0530

    language translator added

---
+++ src/content/en.js
@@ -0,0 +1,17 @@
+export default {
+    main_menu:'MAIN MENU',
+    sub_menu_dashboard:"Dashboard",
+    sub_menu_market:"Market",
+    customer_menu:"CUSTOMERS",
+    sub_customer_list:"Customer List",
+    sub_customer_account:"Account",
+    trading_menu:"TRADING",
+    sub_trading_addorder:"Add order",
+    sub_trading_orderlist:"Order list",
+    sub_trading_history:'Trading history',
+    withdraw:"WITHDRAW",
+    report:"REPORT",
+    ipo:"IPO",
+    content:"CONTENT",
+
+}
